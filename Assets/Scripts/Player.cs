786d10a3fb625d84c2f3160291576577198b7290
Derek Haus <rumbleincybertron@gmail.com>
2018-08-14 21:12:36 +0900

    Prevent Wall Jump
    Added box collider to the feet. Then set the capsule collider on the body to prevent jumping when the player's body touchs a wall in the foreground.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,8 @@ public class Player : MonoBehaviour
     // Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    CapsuleCollider2D myBodyCollider2D;
+    BoxCollider2D myFeet;
     float gravityScaleAtStart;
 
     // Message then methods
@@ -23,7 +24,8 @@ public class Player : MonoBehaviour
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidBody.gravityScale;
 	}
 	
@@ -48,7 +50,7 @@ public class Player : MonoBehaviour
 
     private void ClimbLadder()
     {
-        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             myAnimator.SetBool("Climbing", false);
             myRigidBody.gravityScale = gravityScaleAtStart;
@@ -66,7 +68,7 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
 
         if(Input.GetButtonDown("Jump"))
         {

ef2739cc007b7d1ff0cba114a2337a422fc31dda
Derek Haus <rumbleincybertron@gmail.com>
2018-08-14 15:03:24 +0900

    Player Movement (Horizontal)
    Created player class in which a method called Run() that handles running was implemented.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Run();
+	}
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");   // value is between -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}

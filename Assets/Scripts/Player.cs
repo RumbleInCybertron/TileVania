6ab3d87ffb1b99fa65611c2806b7de94c879ed8b
Derek Haus <rumbleincybertron@gmail.com>
2018-08-14 15:12:22 +0900

    Flip Character Sprite
    added FlipSprite() method to Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,7 @@ public class Player : MonoBehaviour
 	void Update()
     {
         Run();
+        FlipSprite();
 	}
 
     private void Run()
@@ -26,5 +27,14 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal");   // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }

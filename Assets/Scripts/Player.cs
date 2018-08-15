0868efb4e8cb65240e63c1152dd095d89f32c6d3
Derek Haus <rumbleincybertron@gmail.com>
2018-08-15 10:54:39 +0900

    Added Hazards
    Hazards layer added to Tilemap. Player dies upon touching any hazards in scene.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -82,7 +82,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
-        if(myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if(myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Die");

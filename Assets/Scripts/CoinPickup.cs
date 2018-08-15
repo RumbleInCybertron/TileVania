d1abc69733e89c25d8dc2587a3a5175aac12eb15
Derek Haus <rumbleincybertron@gmail.com>
2018-08-15 19:36:29 +0900

    Coin Pickups

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    CapsuleCollider2D myBodyCollider2D;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
